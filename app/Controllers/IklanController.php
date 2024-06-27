49dfa00ccf828ad4b8daf5b71e0641940d6b85b8
Helminafan <nafan2332@gmail.com>
2024-06-27 08:35:08 +0800

    add iklan

--- app/Controllers/IklanController.php
+++ app/Controllers/IklanController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\IklanTetap;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
@@ -19,10 +20,10 @@ class IklanController extends BaseController
         $data = [
             'menu' => 'iklan',
             'sub_menu' => 'iklan_tetap',
-            'iklan'=> $this->iklantetap->getIklanTetap()
+            'iklan' => $this->iklantetap->getIklanTetap()
         ];
-       
-        return view('admin/iklan_tetap/view_iklan_tetap',$data);
+
+        return view('admin/iklan_tetap/view_iklan_tetap', $data);
     }
 
     public function add_iklan_tetap()
@@ -31,25 +32,31 @@ class IklanController extends BaseController
             'menu' => 'iklan',
             'sub_menu' => 'iklan_tetap'
         ];
-        return view('admin/iklan_tetap/tambah_iklan_tetap',$data);
+        return view('admin/iklan_tetap/tambah_iklan_tetap', $data);
     }
 
-    public function store_iklan_tetap()  {
+    public function store_iklan_tetap()
+    {
 
+        $totalIklanTetap = $this->iklantetap->countAll();
+        if ($totalIklanTetap >= 4) {
+            session()->setFlashdata('error', 'Hanya dapat menambahkan maksimal 4 iklan tetap.');
+            return redirect()->to('/admin/add_iklan_tetap');
+        }
         if (!$this->validate([
             'judul_iklan' => 'required'
         ])) {
             $validation = \Config\Services::validation();
-           return redirect()->to('admin/add_iklan_tetap')->withInput()->with('validation',$validation);
+            return redirect()->to('admin/add_iklan_tetap')->withInput()->with('validation', $validation);
         }
-        $slug = url_title($this->request->getVar('judul_iklan'),'-',true);
+        $slug = url_title($this->request->getVar('judul_iklan'), '-', true);
         $foto_iklan = $this->request->getFile('foto_iklan');
         $foto_iklan->move('img');
         $nama_foto = $foto_iklan->getName();
         $this->iklantetap->save([
             'foto_iklan' => $nama_foto,
             'isi_iklan' => $this->request->getVar('isi_iklan'),
-            'slug'=> $slug,
+            'slug' => $slug,
             'judul_iklan' => $this->request->getVar('judul_iklan'),
         ]);
         session()->setFlashdata('pesan', 'data berhasil ditambah');
@@ -61,46 +68,48 @@ class IklanController extends BaseController
     {
         session();
         $data = [
-            'validation'=>\Config\Services::validation(),
-            'iklan'=> $this->iklantetap->getIklantetap($slug),
+            'validation' => \Config\Services::validation(),
+            'iklan' => $this->iklantetap->getIklantetap($slug),
             'menu' => 'iklan',
             'sub_menu' => 'iklan_tetap'
 
         ];
-        return view('admin/iklan_tetap/edit_iklan_tetap',$data);
+        return view('admin/iklan_tetap/edit_iklan_tetap', $data);
     }
 
-    public function update_iklan_tetap($id)  {
+    public function update_iklan_tetap($id)
+    {
         $foto = $this->request->getFile('foto_iklan');
 
         if ($foto->getError() == 4) {
             $this->request->getVar('foto_lama');
         } else {
             $foto->move('img');
-            unlink('img/'.$this->request->getVar('foto_lama'));
+            unlink('img/' . $this->request->getVar('foto_lama'));
         }
-        
+
         if (!$this->validate([
             'judul_iklan' => 'required'
         ])) {
             $validation = \Config\Services::validation();
-           return redirect()->to('admin/edit_iklan_tetap'.$this->request->getVar('slug'))->withInput()->with('validation',$validation);
+            return redirect()->to('admin/edit_iklan_tetap' . $this->request->getVar('slug'))->withInput()->with('validation', $validation);
         }
-        $slug = url_title($this->request->getVar('judul_iklan'),'-',true);
+        $slug = url_title($this->request->getVar('judul_iklan'), '-', true);
         $this->iklantetap->save([
-            'id'=>$id,
+            'id' => $id,
             'foto_iklan' => $foto->getName(),
             'isi_iklan' => $this->request->getVar('isi_iklan'),
-            'slug'=> $slug,
+            'slug' => $slug,
             'judul_iklan' => $this->request->getVar('judul_iklan'),
         ]);
         session()->setFlashdata('pesan', 'data berhasil diedit');
         return redirect()->to('/admin/view_iklan_tetap');
     }
 
-    public function delete_iklan_tetap($id){
+    public function delete_iklan_tetap($id)
+    {
         $foto = $this->iklantetap->find($id);
-        unlink('img/'. $foto['foto_iklan']);
+        unlink('img/' . $foto['foto_iklan']);
         $this->iklantetap->delete($id);
         return redirect()->to('/admin/view_iklan_tetap');
     }
