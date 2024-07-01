43e291412b61b2c2e1d740160b3e3de224819fcf
selamat maulana <setiawangobit1@gmail.com>
2024-07-01 14:17:55 +0800

    update_Recent-Product

--- app/Views/user/home.php
+++ app/Views/user/home.php
@@ -264,7 +264,7 @@
                         <div class="text-center py-4 ">
                             <p class="h6 text-decoration-none text-truncate"><?= $bk['judul_barang']; ?></p>
                             <div class="justify-content-center mt-2">
-                                <h5 class="mb-0 ">Rp. <?= number_format($bk['harga_barang'],0, ',', '.'); ?></h5>
+                                <h5 class="mb-0 ">Rp. <?= number_format($bk['harga_barang'], 0, ',', '.'); ?></h5>
                             </div>
                         </div>
                     </a>
@@ -313,32 +313,28 @@
 <div class="container-fluid pt-5 pb-3">
     <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Recent Products</span></h2>
     <div class="row px-xl-5">
-
         <?php foreach ($barang_baru as $bb) : ?>
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
-                    <div class="product-img position-relative overflow-hidden">
-                        <img class="" style="object-fit: scale-down !important; width: 280px; height: 280px;" src="<?= base_url(); ?>barang/<?= $bb['foto_barang']; ?>" alt="">
-                        <div class="product-action">
-                            <button class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></button>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
+                    <a href="<?= base_url(); ?>user/detail/<?= $bk['id']; ?>" class="d-block text-decoration-none">
+                        <div class="product-img position-relative overflow-hidden">
+                            <img class="" style="object-fit: scale-down !important; width: 280px; height: 280px;" src="<?= base_url(); ?>barang/<?= $bb['foto_barang']; ?>" alt="">
                         </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href=""><?= $bb['judul_barang']; ?></a>
-                        <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>Rp. <?= number_format($bb['harga_barang'],0, ',', '.'); ?></h5>
-                            <h6 class="text-muted ml-2"><del>$123.00</del></h6>
+                        <div class="text-center py-4">
+                            <a class="h6 text-decoration-none text-truncate" href=""><?= $bb['judul_barang']; ?></a>
+                            <div class="d-flex align-items-center justify-content-center mt-2">
+                                <h5>Rp. <?= number_format($bb['harga_barang'], 0, ',', '.'); ?></h5>
+                                <h6 class="text-muted ml-2"><del>123.000</del></h6>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-center mb-1">
+                                <small class="fa fa-star text-primary mr-1"></small>
+                                <small class="fa fa-star text-primary mr-1"></small>
+                                <small class="fa fa-star text-primary mr-1"></small>
+                                <small class="fa fa-star text-primary mr-1"></small>
+                                <small class="fa fa-star text-primary mr-1"></small>
+                                <small>(99)</small>
+                            </div>
                         </div>
-                        <div class="d-flex align-items-center justify-content-center mb-1">
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
                 </div>
             </div>
         <?php endforeach; ?>
